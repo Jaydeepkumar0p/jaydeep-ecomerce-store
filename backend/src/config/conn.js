e49ccacb7326587b4c5d5d549c8008290776cc7d
jaydeep kumar <118693035+Jaydeepkumar0p@users.noreply.github.com>
2025-11-26 01:02:15 +0530

    Enhance database connection error handling
    Added error handling for undefined MONGODB_URL.

--- backend/src/config/conn.js
+++ backend/src/config/conn.js
@@ -1,15 +1,18 @@
-import mongoose from "mongoose";
-
-const Connection=async()=>{
-    try{
-        const conn=await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`Database connected successfully at ${conn.connection.host}`);
-    }
-    catch(err){
-        console.log("Error while connecting with the database",err.message);
+// config/conn.js
+const Connection = async () => {
+    // Check if the URL is defined
+    if (!process.env.MONGODB_URL) {
+        console.error("Error: MONGODB_URL is not defined in environment variables.");
         process.exit(1);
-        
     }
-
+    
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`Database connected successfully at ${conn.connection.host}`);
+    } 
+    catch(err) {
+        console.log("Error while connecting with the database", err.message);
+        process.exit(1);
+    }
 }
 export default Connection;
